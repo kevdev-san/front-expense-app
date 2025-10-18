ec6c8c2da5d4c42304a712c648ec0eb40960aa7b
Kevin <kbanossanchez@gmail.com>
2025-10-18 13:08:46 -0600

    arreglo de urls

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,7 +4,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL; //Aqui definimos la url de nues
 
 //hacemos una funcion para registrar los usuarios, definimos el tipo que es un objeto que se llama data, el cual tendra atributos que son los que estaremos pidiendo y tipamos el tipo de dato que vamos a estar esperando, despues hacemos una peticion con fetch y la guardamos en la constante res, dentro del fetch pondremos la url que definimos en api url, mas lo que le sigue segun el nombre o la url que hayamos hecho en el back para registrar los usuarios y mediante un metodo post mandaremos los datos que el usuario ingrese para hacer el registro del usuario
 export async function registerUser(data: {username: string; email: string; password: string;}){
-    const res = await fetch (`${API_URL}/register/`, {
+    const res = await fetch (`${API_URL}/api/register/`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(data),
@@ -16,7 +16,7 @@ export async function registerUser(data: {username: string; email: string; passw
 
 //creamos una funcion para que el usuario haga login mediante su contraseña y correo
 export async function loginUser(data: { username: string; password: string }): Promise<{ access: string; refresh: string }> {
-    const res = await fetch(`${API_URL}/token/`, {
+    const res = await fetch(`${API_URL}/api/token/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
